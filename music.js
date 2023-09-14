2030c2aadf96d13bcb28f298b8d7ff6faa2c7aa1
Juan Jose A. Zuluaga <juaninfante29@gmail.com>
2023-09-14 16:36:30 -0500

    avanced day to day

--- music.js
+++ music.js
@@ -1,46 +1,3 @@
-// API SPOTIFY
-
-const apiMusic = axios.create({
-    baseURL: 'https://spotify117.p.rapidapi.com',
-    headers:{
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '6c1967e5d3msh3257be7d6cac589p1ff725jsnbccdc806cd80',
-        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-    }
-})
-
-let inputMusic = document.getElementById('input-music')
-
-async function petitionAlbum(){
-    const {data,status} = await apiMusic.get('/albums/?ids=3IBcauSj5M2A6lTeffJzdv')
-    const sectionAlbum = document.getElementById('section-album')
-    const imgAlbum = document.createElement('img')
-    let pAlbum = document.createElement('p')
-    let listTrack = document.createElement('p')
-    let playTrack = document.createElement('a')
-    const playText = document.createTextNode("Play")
-    sectionAlbum.appendChild(pAlbum)
-    sectionAlbum.appendChild(imgAlbum)
-    sectionAlbum.appendChild(listTrack)
-    sectionAlbum.appendChild(playTrack)
-    playTrack.appendChild(playText)
-
-    if(status == 200){
-        data.albums.map(item => {
-            imgAlbum.src = item.images[0].url
-        })
-        data.albums.map(item => {
-            pAlbum.innerHTML =  `Album:${item.name} Artist:${item.artists[0].name}`
-        })
-        data.albums.forEach(item => {
-            listTrack.innerHTML = item.tracks.items.map(item => `<li>${item.name}</li>`)    
-        })
-        data.albums.map(item => {
-            playTrack.href = item.external_urls.spotify
-        })
-    }
-}
-
 // API MUSIC FREE
 
 const apiMusicFree = axios.create({
@@ -60,24 +17,48 @@ async function infoApiMusic(){
 
 // Vamos bien, debo encontrar la solucion para cuando le de click a search aparezca un boton con la opcion de darle play y sepa de inmediato que cancion es, por medio del id
 
-let inputMusicFree = document.getElementById('input-music-free')
+let inputSearchArtist = document.getElementById('input-search-artist')
 async function searchMusicFree(){
-    const {data,status} = await apiMusicFree.get(`/search?q=emienm`)//${inputMusicFree.value}
-    const sectionMusicFree = document.getElementById('section-music-free')
-    const listTracks = document.createElement('p')
-    sectionMusicFree.appendChild(listTracks)
+    const {data,status} = await apiMusicFree.get(`/search?q=${inputSearchArtist.value}`)//
+    const sectionMusicFree = document.getElementById('section-search-music')
 
     if(status == 200){
-        
-        listTracks.innerHTML = data.data.map(item => `<li>${item.title}</li>`)
-        console.log(data.data.map(item => `${item.title} ${item.id}`))
+        data.data.filter(item => {
+            const articleMusic = document.createElement('article')
+            const listArtist = document.createElement('p')
+            const linkTracks = document.createElement('a')
+            const textLinkTracks = document.createTextNode('Play')
+            const imgTrack = document.createElement('img')
+            linkTracks.appendChild(textLinkTracks)
+            listArtist.innerHTML = item.title
+            linkTracks.href = item.link
+            imgTrack.src = item.album.cover_medium
+            articleMusic.appendChild(listArtist)
+            articleMusic.appendChild(imgTrack)
+            articleMusic.appendChild(linkTracks)
+            sectionMusicFree.appendChild(articleMusic)
+            console.log(data) 
+        })
     }
 }
 
-async function trackMusicFree(){
-    const{data,status} = await apiMusicFree.get(`/track/1579904`)
+let inputArtist = document.getElementById('input-artist')
+async function searchArtist(){
+    const{data,status} = await apiMusicFree.get(`/artist/${inputArtist.value}`)
+    const sectionArtist = document.getElementById('section-search-artist')
 
     if(status == 200){
+        const imgArtist = document.createElement('img')
+        const nameTrack = document.createElement('p')
+        const linkListenTrack = document.createElement('a')
+        const textLinkListenTrack = document.createTextNode('Albums')
+        linkListenTrack.appendChild(textLinkListenTrack)
+        nameTrack.innerText = data.name
+        imgArtist.src = data.picture_medium
+        linkListenTrack.href = data.link
+        sectionArtist.appendChild(imgArtist)
+        sectionArtist.appendChild(linkListenTrack)
+        sectionArtist.appendChild(nameTrack)
         console.log(data)
     }else{
         alert('FALLA')
